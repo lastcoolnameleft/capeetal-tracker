25386a3e80d58717fdae75cecb44bd5c599a11c5
Tommy Falgout <tommy@lastcoolnameleft.com>
2024-05-31 08:58:25 -0500

    fix empty states

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,16 @@ router.get('/', function(req, res, next) {
   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   var states = req.query['active'];
 
+  var imgPath;
   // Expecting a string like 'US-CA,US-NY'
   if (states) {
     states = states.split(',').map(state => state.slice(3).toUpperCase());
     states = stateHelper.filterStates(states);
+    imgPath = '/map/us/' + states.join('-') + '.png';
   } else {
     states = [];
+    imgPath = '/map/us/empty.png';
   }
-  imgPath = '/map/us/' + states.join('-') + '.png';
   var imgUrl = req.protocol + '://' + req.get('host') + imgPath;
 
   var stateListStr = states.join(', ');
