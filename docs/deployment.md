9ce7b2ce83d41148d846426fed2cb2d56262464e
Tommy Falgout <tommy@lastcoolnameleft.com>
2024-07-14 22:49:51 -0500

    get sqlite db right

--- docs/deployment.md
+++ docs/deployment.md
@@ -2,6 +2,12 @@
 
 ```shell
 # https://github.com/Wowu/docker-rollout
+sudo su ghaction
+cd /home/ghaction/capeetal-tracker
+docker compose --file docker-compose-stg.yaml --env-file staging.env up
+# OR
+docker rollout -f docker-compose-stg.yaml --env-file staging.env capeetal-tracker-stg
+
 docker rollout -f ./docker-compose.yaml capeetal-tracker-prod
 
 docker compose --file ~/capeetal-tracker/docker-compose.yaml up -d
--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,12 @@ router.post('/save', function(req, res, next) {
   const session = req.body.session;
   const locations = req.body.locations;
   const region = req.body.region;
+  const dbPath = process.env.SQLITE_DB_PATH + region.toLowerCase() + '.db';
   console.log('region:', region);
   console.log('session:', session);
   console.log('locations:', locations);
-  const dbPath = process.env.SQLITE_DB_PATH;
-  const db = new sqlite3.Database(dbPath + region + '.db');
+  console.log('dbPath:', dbPath);
+  const db = new sqlite3.Database(dbPath);
   db.run(`INSERT OR REPLACE INTO locations(session, last_update, locations) VALUES(?, CURRENT_TIMESTAMP, ?)`, 
       [session, locations],
       function(error){
